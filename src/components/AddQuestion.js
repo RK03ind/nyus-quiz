291983a108d14d29b37c9a1242d95bb2f6aeb4fe
RK03ind <gamitech95@gmail.com>
2022-05-01 21:25:29 +0530

    implemented form validation

--- src/components/AddQuestion.js
+++ src/components/AddQuestion.js
@@ -1,21 +1,25 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { QuestionItem } from "./QuestionItem";
 import styles from "./styles/AddQuestion.module.css";
 
 export const AddQuestion = () => {
+  const [isInvalid, setInvalidity] = useState(false);
   let questionData = [{}, {}];
   const formRef = useRef();
 
   const onSubmit = (e) => {
     e.preventDefault();
     console.log(formRef.current.checkValidity());
+    if (!formRef.current.checkValidity()) {
+      setInvalidity(true);
+    }
   };
 
   return (
     <form className={styles.questions} ref={formRef}>
-      <input type="date" required />
+      <input type="date" required className={styles.date} />
       {questionData.map((item, index) => {
-        return <QuestionItem qNumber={index + 1} />;
+        return <QuestionItem qNumber={index + 1} isInvalid={isInvalid} />;
       })}
       <button type="submit" onClick={onSubmit}></button>
     </form>
--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -6,23 +6,45 @@ export const QuestionItem = (props) => {
       <header>Question {props.qNumber}</header>
       <input
         type="text"
-        className={styles.question}
+        className={`${styles.question} ${
+          props.isInvalid ? styles.validity : ""
+        }`}
         required
         placeholder="Question"
       />
       <div className={styles.options}>
-        <input type="text" required placeholder="Option 1" />
-        <input type="text" required placeholder="Option 2" />
-        <input type="text" required placeholder="Option 3" />
-        <input type="text" required placeholder="Option 4" />
+        <input
+          type="text"
+          required
+          className={`${props.isInvalid ? styles.validity : ""}`}
+          placeholder="Option 1"
+        />
+        <input
+          type="text"
+          required
+          className={`${props.isInvalid ? styles.validity : ""}`}
+          placeholder="Option 2"
+        />
+        <input
+          type="text"
+          className={`${props.isInvalid ? styles.validity : ""}`}
+          required
+          placeholder="Option 3"
+        />
+        <input
+          type="text"
+          className={`${props.isInvalid ? styles.validity : ""}`}
+          required
+          placeholder="Option 4"
+        />
       </div>
       <select
         name="answer"
         placeholder="Select Answer"
-        className={styles.answer}
         required
+        className={`${styles.answer} ${props.isInvalid ? styles.validity : ""}`}
       >
-        <option disabled selected hidden>
+        <option value="" hidden>
           Select Answer
         </option>
         <option>Option 1</option>
@@ -31,7 +53,7 @@ export const QuestionItem = (props) => {
         <option>Option 4</option>
       </select>
       <textarea name="hint" className={styles.textarea} placeholder="Hint" />
-      <button>Delete</button>
+      <button className={styles.delete}>Delete</button>
     </div>
   );
 };
