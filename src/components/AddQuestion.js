bfc93b5f491afbcb8eebcfaea9b606afe326b90b
RK03ind <gamitech95@gmail.com>
2022-05-01 15:14:09 +0530

    design implemented

--- src/components/AddQuestion.js
+++ src/components/AddQuestion.js
@@ -1,13 +1,23 @@
+import { useRef } from "react";
 import { QuestionItem } from "./QuestionItem";
+import styles from "./styles/AddQuestion.module.css";
 
 export const AddQuestion = () => {
-  let questionData = [{}];
+  let questionData = [{}, {}];
+  const formRef = useRef();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(formRef.current.checkValidity());
+  };
 
   return (
-    <main>
-      {questionData.map(() => {
-        return <QuestionItem />;
+    <form className={styles.questions} ref={formRef}>
+      <input type="date" required />
+      {questionData.map((item, index) => {
+        return <QuestionItem qNumber={index + 1} />;
       })}
-    </main>
+      <button type="submit" onClick={onSubmit}></button>
+    </form>
   );
 };
--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,23 +1,37 @@
+import styles from "./styles/QuestionItem.module.css";
+
 export const QuestionItem = (props) => {
   return (
-    <div className="question-item">
-      <input type="date" />
-      <header>Question 1</header>
-      <input type="text" className="question" />
-      <div className="options">
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
+    <div className={styles.question__item}>
+      <header>Question {props.qNumber}</header>
+      <input
+        type="text"
+        className={styles.question}
+        required
+        placeholder="Question"
+      />
+      <div className={styles.options}>
+        <input type="text" required placeholder="Option 1" />
+        <input type="text" required placeholder="Option 2" />
+        <input type="text" required placeholder="Option 3" />
+        <input type="text" required placeholder="Option 4" />
       </div>
-      <select name="answer">
+      <select
+        name="answer"
+        placeholder="Select Answer"
+        className={styles.answer}
+        required
+      >
+        <option disabled selected hidden>
+          Select Answer
+        </option>
         <option>Option 1</option>
         <option>Option 2</option>
         <option>Option 3</option>
         <option>Option 4</option>
       </select>
-
-      <textarea name="hint" />
+      <textarea name="hint" className={styles.textarea} placeholder="Hint" />
+      <button>Delete</button>
     </div>
   );
 };
